ee44d16bc85c31eba2e040c706ee4dfcc50a1c7c
Andy Wu <andywu@Andys-Mac.local>
2017-02-14 15:42:56 -0500

    Added number pad and delete btn

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var inputLbl: UITextField!
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    
+    @IBAction func numPressed(_ sender: UIButton) {
+        //print("Num Pressed: \((sender.titleLabel?.text)!)")
+        let numInput = (sender.titleLabel?.text)!
+        let currentNum = (inputLbl.text!)
+        inputLbl.text = "\(currentNum + numInput)"
+        
+    }
+    
+    @IBAction func deleteBtn() {
+        var delFromInput = inputLbl.text!
+        delFromInput.remove(at: delFromInput.index(before: delFromInput.endIndex))
+        inputLbl.text = delFromInput
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
